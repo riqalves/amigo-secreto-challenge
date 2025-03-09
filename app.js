932ce061438d407dee583853e191293a1bf04603
riqalves <henrique12095@gmail.com>
2025-03-09 06:08:34 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let arrayNomes = [];
+
+//* capturar valor
+let campoNome = document.querySelector('#amigo');
+let listaNomes = document.querySelector('#listaAmigos')
+let resultado = document.querySelector('#resultado')
+
+
+function limparLista(){
+    listaNomes.remove(listaNomes)
+}
+
+function limparResultado(){
+    resultado.remove(resultado)
+}
+
+function atualizarListaAmigos(arrayNomes){
+
+    //* Exibe o ultimo nome adicionado
+    let ultimoNome = arrayNomes[arrayNomes.length -1]
+    listaNomes.innerHTML += `<li>Lista: ${ultimoNome}</li>`
+}
+
+function adicionarAmigo(){
+    
+    //* validação 
+    if(campoNome.value == "" || campoNome.value == " ")
+    { return alert("por favor, escreva um nome")}
+    
+    //* adicionar numa lista
+    arrayNomes.push(campoNome.value);
+
+    //* limpar campo de entrada
+    campoNome.value = "";
+
+    atualizarListaAmigos(arrayNomes)
+
+}
+
+function gerarNumeroAleatorio(tamanhoLista){
+    let numeroEscolhido = parseInt(Math.random() * tamanhoLista + 1)
+    return numeroEscolhido
+}
+
+function sortearAmigo(){
+    if(arrayNomes.length == 0) {return alert ("adicione um nome antes")} 
+    numeroEscolhido = gerarNumeroAleatorio(arrayNomes.length)
+    console.log(numeroEscolhido)
+    console.log("O escolhido foi: " + arrayNomes[numeroEscolhido-1])
+    limparLista(arrayNomes)
+    resultado.innerHTML += `<li>O escolhido: ${arrayNomes[numeroEscolhido-1]}</li>`
+}
